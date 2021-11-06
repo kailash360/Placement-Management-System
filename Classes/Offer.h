51733e02b0d61da050bc2ac39bc863d430978e03
kailash360 <kailashkejriwal21@gmail.com>
2021-11-06 19:19:40 +0530

    Added class for offer

---
+++ Classes/Offer.h
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+#ifndef OFFER_H
+#define OFFER_H
+
+class Offer{
+    private:
+        int amount; 
+        string location;
+        string type;
+    public:
+        Offer(int _amount, string _location, string _type){
+            amount = _amount;
+            location = _location;
+            type = _type;
+        }
+
+        void setAmount(int _amount){
+            amount = _amount;
+        }
+
+        void setLocation(string _location){
+            location = _location;
+        }
+
+        void setType(string _type){
+            type = _type;
+        }
+};
+
+#endif

fe309511febd4e916ce8d62281c57a15470f2674
kailash360 <kailashkejriwal21@gmail.com>
2021-11-06 19:46:25 +0530

    Removed default constructor

--- Classes/Offer.h
+++ Classes/Offer.h
@@ -10,21 +10,15 @@ class Offer{
         string location;
         string type;
     public:
-        Offer(int _amount, string _location, string _type){
-            amount = _amount;
-            location = _location;
-            type = _type;
-        }
-
-        void setAmount(int _amount){
+        void set_amount(int _amount){
             amount = _amount;
         }
 
-        void setLocation(string _location){
+        void set_location(string _location){
             location = _location;
         }
 
-        void setType(string _type){
+        void set_type(string _type){
             type = _type;
         }
 };

0da131ab44144f73be8236fc0897c4aeb8dffabe
Vishwas Singh <75408888+vishwas031@users.noreply.github.com>
2021-11-08 22:30:13 +0530

    Update programs.h

--- Classes/programs.h
+++ Classes/programs.h
@@ -2,34 +2,33 @@
 
 using namespace std;
 
-class Program
-{
-private:
-    string program_name, type, seats;
-    int duration;
+class Program{
+    private:
+        string program_name, type, seats;
+        int duration;
 
-public:
-    Program(string _program_name, int _duration, string _type)
-    {
-        program_name = _program_name;
-        duration = _duration;
-        type = _type;
-    }
-    void get_program_name()
-    {
-        cout << program_name << endl;
-    }
-    void get_duration()
-    {
-        cout << duration << endl;
-    }
-    void get_type()
-    {
-        cout << type << endl;
-    }
-    void set_seats()
-    {
-        cin >> seats;
-    }
-    friend class Student;
+    public:
+        Program(string _program_name, int _duration, string _type)
+        {
+            program_name = _program_name;
+            duration = _duration;
+            type = _type;
+        }
+        void get_program_name()
+        {
+            cout << program_name << endl;
+        }
+        void get_duration()
+        {
+            cout << duration << endl;
+        }
+        void get_type()
+        {
+            cout << type << endl;
+        }
+        void set_seats()
+        {
+            cin >> seats;
+        }
+        friend class Student;
 };

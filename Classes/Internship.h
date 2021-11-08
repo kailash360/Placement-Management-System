1ba10d6c04cbd66e536693f3a02577ecbd585e94
gaurav879 <gaurav91306@gmail.com>
2021-11-08 20:25:44 +0530

    Made changes in job and internship changed the inheritance of internship

--- Classes/Internship.h
+++ Classes/Internship.h
@@ -1,32 +1,38 @@
-#include "Offer.h"
-
+#include "Job.h"
 #ifndef INTERNSHIP_H
 #define INTERNSHIP_H
 
-class Internship:public Offer{
-    private:
-        int program_count;
-    public:
-        Internship(int _amount, string _location, string _type,int _program_count){
-            amount = _amount;
-            location = _location;
-            type = _type;
-            program_count = _program_count;
-        }
+class Internship : public Job
+{
+private:
+    int program_count;
 
-        void set_program_count(int _program_count){
-            program_count = _program_count;
-        }
+public:
+    Internship(int _amount, string _location, string _type, string _role, int _program_count)
+    {
+        amount = _amount;
+        location = _location;
+        type = _type;
+        role = _role;
+        program_count = _program_count;
+    }
 
-        friend ostream& operator<<(ostream& out, Internship& internship);
+    void set_program_count(int _program_count)
+    {
+        program_count = _program_count;
+    }
+    friend class Company; 
+    friend ostream &operator<<(ostream &out, Internship &internship);
 };
 
-ostream& operator<<(ostream& out,Internship& internship){
-    cout<<"\t|| INTERNSHIP DETAILS ||"<<endl;
-    cout<<"Amount: "<<internship.amount<<endl;
-    cout<<"Location: "<<internship.location<<endl;
-    cout<<"Type: "<<internship.type<<endl;
-    cout<<"Program Count: "<<internship.program_count<<endl;
+ostream &operator<<(ostream &out, Internship &internship)
+{
+    cout << "\t|| INTERNSHIP DETAILS ||" << endl;
+    cout << "Amount: " << internship.amount << endl;
+    cout << "Location: " << internship.location << endl;
+    cout << "Type: " << internship.type << endl;
+    cout << "Role: " << internship.role << endl;
+    cout << "Program Count: " << internship.program_count << endl;
 }
 
 #endif

707411cf12986d181140717145ac6ab9dc92570f
kailash360 <kailashkejriwal21@gmail.com>
2021-11-09 19:42:39 +0530

    Added read fle for job and nternship

--- Utils/File.h
+++ Utils/File.h
@@ -1,14 +1,17 @@
 #ifndef FILE_H
 #define FILE_H
+#include<fstream>
 #include <bits/stdc++.h>
 using namespace std;
 #include "../Classes/Student.h"
-#include "../Classes/Company.cpp"
+#include "../Classes/Company.h"
 #include "../Classes/College.h"
+#include "../Classes/Job.h"
+#include "../Classes/Internship.h"
 
 
 void tokenize_student(string s,vector<Student> &data){
-    
+
     // Vector of string to save tokens
     vector <string> row;
       
@@ -27,6 +30,42 @@ void tokenize_student(string s,vector<Student> &data){
     data.push_back(*student);
 }
 
+void tokenize_job(string s,vector<Job> &data){
+    // Vector of string to save tokens
+    vector <string> row;
+      
+    // stringstream class check1
+    stringstream check1(s);
+    string intermediate;
+      
+    // Tokenizing w.r.t. ','
+    while(getline(check1, intermediate, ',')){
+        row.push_back(intermediate);
+    }
+
+    //Push the tokens to the data
+    Job* job = new Job(stoi(row[0]),row[1],row[2],stoi(row[3]),row[4]);
+    data.push_back(*job);
+}
+
+void tokenize_internship(string s,vector<Internship> &data){
+    // Vector of string to save tokens
+    vector <string> row;
+      
+    // stringstream class check1
+    stringstream check1(s);
+    string intermediate;
+      
+    // Tokenizing w.r.t. ','
+    while(getline(check1, intermediate, ',')){
+        row.push_back(intermediate);
+    }
+
+    //Push the tokens to the data
+    Internship* internship = new Internship(stoi(row[0]),row[1],row[2],row[3],stoi(row[4]));
+    data.push_back(*internship);
+}
+
 void tokenize_company(string s,vector<Company> &data){
     
     // Vector of string to save tokens
@@ -42,8 +81,27 @@ void tokenize_company(string s,vector<Company> &data){
         row.push_back(intermediate);
     }
 
+    //Read the file of jobs
+    vector <Job> jobs;
+    fstream fin2;
+    fin2.open("./Data/Job.csv");
+    while(!fin2.eof()){
+        fin2>>intermediate;
+        tokenize_job(intermediate,jobs);
+    }
+    fin2.close();
+
+    //Read the file of internships
+    vector <Internship> internships;
+    fin2.open("./Data/Internship.csv");
+    while(!fin2.eof()){
+        fin2>>intermediate;
+        tokenize_internship(intermediate,internships);
+    }
+    fin2.close();
+
     //Push the tokens to the data
-    Company* company = new Company(row[0],row[1],row[2]);
+    Company* company = new Company(row[0],row[1],row[2],jobs,internships);
     data.push_back(*company);
 }
 
@@ -69,7 +127,7 @@ void tokenize_college(string s,vector<College> &data){
 
 void read_file(vector<Student> &students,vector<Company> &companies,vector<College> &colleges){
     
-    fstream fin,fout;
+    ifstream fin;
     string line;
 
     //Read data of students

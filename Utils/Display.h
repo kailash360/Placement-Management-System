0b4979424f8926314cbe4b5ec899d4a0a4c3c500
SukhveerS <78963782+Rexbeast2@users.noreply.github.com>
2021-11-11 16:39:56 +0530

    Update Display.h

--- Utils/Display.h
+++ Utils/Display.h
@@ -12,7 +12,7 @@ void display_line(int choice)
     switch (choice)
     {
     case 1:
-        cout<<"=================================================================="<<endl;
+        cout<<"\t\t\t=================================================================="<<endl;
         break;
     case 2:
         cout << "-------------------------------------------------------------" << endl;
@@ -24,10 +24,10 @@ void display_line(int choice)
 
 void display_students(vector<Student> &Students)
 {
-    cout << "\t  || LIST OF STUDENTS ||\n"
+    cout << "\t\t\t\t\t  || LIST OF STUDENTS ||\n"
          << endl;
-    cout.width(5);
-    cout << "Serial No."
+    cout.width(3);
+    cout << "\t\t\t\t| Serial No."
          << "|";
     cout.width(12);
     cout << "Name"
@@ -36,13 +36,13 @@ void display_students(vector<Student> &Students)
     cout << "Roll Number"
          << "\t|";
     cout << endl;
-    cout<<"-------------------------------------------------"<<endl;
+    cout<<"\t\t\t\t-------------------------------------------------"<<endl;
 
     int index = 1;
     for (auto student : Students)
-    {
-        cout.width(5);
-        cout << index++ << " \t  |";
+    {   cout<<"\t\t\t\t";
+        cout.width(3);
+        cout << "|   "<<index++ << " \t    |";
         cout.width(15);
         cout << student.name << "\t|";
         cout.width(10);
@@ -52,14 +52,15 @@ void display_students(vector<Student> &Students)
     cout << "\n"
          << endl;
     display_line(1);
+     cout<<"\n\n";
 }
 
 void display_companies(vector<Company> &Companies)
 {
-    cout << "\t  || LIST OF COMPANIES ||\n"
+    cout << "\t\t\t\t\t  || LIST OF COMPANIES ||\n"
          << endl;
-    cout.width(5);
-    cout << "Serial No."
+    cout.width(3);
+    cout << "\t\t\t\t| Serial No."
          << "|";
     cout.width(12);
     cout << "Name"
@@ -68,13 +69,13 @@ void display_companies(vector<Company> &Companies)
     cout << "Location"
          << "\t|";
     cout << endl;
-    cout << "-------------------------------------------------" << endl;
+    cout << "\t\t\t\t-------------------------------------------------" << endl;
 
     int index = 1;
     for (auto company : Companies)
-    {
-        cout.width(5);
-        cout << index++ << " \t  |";
+    {   cout<<"\t\t\t\t";
+        cout.width(3);
+        cout << "|   "<<index++ << " \t    |";
         cout.width(15);
         cout << company.get_name() << "\t|";
         cout.width(10);
@@ -84,14 +85,15 @@ void display_companies(vector<Company> &Companies)
     cout << "\n"
          << endl;
     display_line(1);
+    cout<<"\n\n";
 }
 
 void display_college_list(vector<College> &Colleges)
 {
-    cout << "\t  || LIST OF COLLEGE ||\n"
+    cout << "\t\t\t\t\t  || LIST OF COLLEGE ||\n"
          << endl;
-    cout.width(5);
-    cout << "Serial No."
+     cout.width(3);
+    cout << "\t\t\t\t| Serial No."
          << "|";
     cout.width(12);
     cout << "Name"
@@ -100,13 +102,14 @@ void display_college_list(vector<College> &Colleges)
     cout << "Location"
          << "\t|";
     cout << endl;
-     cout << "-------------------------------------------------" << endl;
+     cout << "\t\t\t\t-------------------------------------------------" << endl;
 
     int index = 1;
     for (auto college : Colleges)
-    {
-        cout.width(5);
-        cout << index++ << " \t  |";
+    {   
+        cout<<"\t\t\t\t";
+        cout.width(3);
+        cout << "|   "<<index++ << " \t    |";
         cout.width(15);
         cout << college.get_name() << "\t|";
         cout.width(10);
@@ -115,40 +118,41 @@ void display_college_list(vector<College> &Colleges)
     }
     cout << endl;
     display_line(1);
+     cout<<"\n\n";
 }
 
 void display_role_text(int student, int company, int college)
 {
     if (student)
     {
-        cout << "   _____  ______   __  __    ____     ______    _   __  ______" << endl;
-        cout << "  / ___/ /_  __/  / / / /   / __ \\   / ____/   / | / / /_  __/" << endl;
-        cout << "  \\__ \\   / /    / / / /   / / / /  / __/     /  |/ /   / /   " << endl;
-        cout << " ___/ /  / /    / /_/ /   / /_/ /  / /___    / /|  /   / /    " << endl;
-        cout << "/____/  /_/     \\____/   /_____/  /_____/   /_/ |_/   /_/     " << endl;
-        cout<<"\n==================================================================\n";
+        cout << "\t\t\t     _____  ______   __  __    ____     ______    _   __  ______" << endl;
+        cout << "\t\t\t    / ___/ /_  __/  / / / /   / __ \\   / ____/   / | / / /_  __/" << endl;
+        cout << "\t\t\t    \\__ \\   / /    / / / /   / / / /  / __/     /  |/ /   / /   " << endl;
+        cout << "\t\t\t   ___/ /  / /    / /_/ /   / /_/ /  / /___    / /|  /   / /    " << endl;
+        cout << "\t\t\t  /____/  /_/     \\____/   /_____/  /_____/   /_/ |_/   /_/     " << endl;
+        cout<<"\n\t\t\t==================================================================\n";
         cout << endl;
     }
 
     if (company)
     {
-        cout << "   ______   ____     __  ___    ____     ___     _   ____  __" << endl;
-        cout << "  / ____/  / __ \\   /  |/  /   / __ \\   /   |   / | / /\\ \\/ /" << endl;
-        cout << " / /      / / / /  / /|_/ /   / /_/ /  / /| |  /  |/ /  \\  / " << endl;
-        cout << "/ /___   / /_/ /  / /  / /   / ____/  / ___ | / /|  /   / /  " << endl;
-        cout << "\\____/   \\____/  /_/  /_/   /_/      /_/  |_|/_/ |_/   /_/   " << endl;
-        cout<<"\n==================================================================\n";
+        cout << "\t\t\t   ______   ____     __  ___    ____     ___     _   ____  __" << endl;
+        cout << "\t\t\t  / ____/  / __ \\   /  |/  /   / __ \\   /   |   / | / /\\ \\/ /" << endl;
+        cout << "\t\t\t / /      / / / /  / /|_/ /   / /_/ /  / /| |  /  |/ /  \\  / " << endl;
+        cout << "\t\t\t/ /___   / /_/ /  / /  / /   / ____/  / ___ | / /|  /   / /  " << endl;
+        cout << "\t\t\t\\____/   \\____/  /_/  /_/   /_/      /_/  |_|/_/ |_/   /_/   " << endl;
+        cout<<"\n\t\t\t==================================================================\n";
         cout << endl;
     }
 
     if (college)
     {
-        cout << "   ______   ____     __     __     ______   ______    ______" << endl;
-        cout << "  / ____/  / __ \\   / /    / /    / ____/  / ____/   / ____/" << endl;
-        cout << " / /      / / / /  / /    / /    / __/    / / __    / __/   " << endl;
-        cout << "/ /___   / /_/ /  / /___ / /___ / /___   / /_/ /   / /___   " << endl;
-        cout << "\\____/   \\____/  /_____//_____//_____/   \\____/   /_____/   " << endl;
-        cout<<"\n==================================================================\n";
+        cout << "\t\t\t   ______   ____     __     __     ______   ______    ______" << endl;
+        cout << "\t\t\t  / ____/  / __ \\   / /    / /    / ____/  / ____/   / ____/" << endl;
+        cout << "\t\t\t / /      / / / /  / /    / /    / __/    / / __    / __/   " << endl;
+        cout << "\t\t\t/ /___   / /_/ /  / /___ / /___ / /___   / /_/ /   / /___   " << endl;
+        cout << "\t\t\t\\____/   \\____/  /_____//_____//_____/   \\____/   /_____/   " << endl;
+        cout<<"\n\t\t\t==================================================================\n";
 
         cout << endl;
     }
@@ -156,36 +160,46 @@ void display_role_text(int student, int company, int college)
 
 void display_project_name()
 {
+    cout<<"\n\n\n\n";
+    cout << "\t\t\t||==============================================================================================||"<<endl;
+    cout << "\t\t\t||\t\t\t\t\t\t\t\t\t\t\t\t||"<<endl;
+    cout << "\t\t\t||\t\t\t\t\t\t\t\t\t\t\t\t||"<<endl;
+    cout << "\t\t\t||\t      %%%%%   %%       %%%%    %%%%   %%%%%%  %%   %%  %%%%%%  %%  %%  %%%%%%    \t||" << endl;
+    cout << "\t\t\t||\t      %%  %%  %%      %%  %%  %%  %%  %%      %%% %%%  %%      %%% %%    %%     \t||" << endl;
+    cout << "\t\t\t||\t      %%%%%   %%      %%%%%%  %%      %%%%    %% % %%  %%%%    %% %%%    %%     \t||" << endl;
+    cout << "\t\t\t||\t      %%      %%      %%  %%  %%  %%  %%      %%   %%  %%      %%  %%    %%     \t||" << endl;
+    cout << "\t\t\t||\t      %%      %%%%%%  %%  %%   %%%%   %%%%%%  %%   %%  %%%%%%  %%  %%    %%     \t||" << endl;
+    cout << "\t\t\t||\t\t\t\t\t\t\t\t\t\t\t\t||"<<endl;
 
-    cout << "%%%%%   %%       %%%%    %%%%   %%%%%%  %%   %%  %%%%%%  %%  %%  %%%%% " << endl;
-    cout << "%%  %%  %%      %%  %%  %%  %%  %%      %%% %%%  %%      %%% %%    %%   " << endl;
-    cout << "%%%%%   %%      %%%%%%  %%      %%%%    %% % %%  %%%%    %% %%%    %%   " << endl;
-    cout << "%%      %%      %%  %%  %%  %%  %%      %%   %%  %%      %%  %%    %%   " << endl;
-    cout << "%%      %%%%%%  %%  %%   %%%%   %%%%%%  %%   %%  %%%%%%  %%  %%    %%   " << endl;
-    cout << endl;
+    cout << "\t\t\t||\t%%   %%   %%%%   %%  %%   %%%%    %%%%   %%%%%%  %%   %%  %%%%%%  %%  %%  %%%%%%  \t||" << endl;
+    cout << "\t\t\t||\t%%% %%%  %%  %%  %%% %%  %%  %%  %%      %%      %%% %%%  %%      %%% %%    %%    \t||" << endl;
+    cout << "\t\t\t||\t%% % %%  %%%%%%  %% %%%  %%%%%%  %% %%%  %%%%    %% % %%  %%%%    %%%%%%    %%    \t||" << endl;
+    cout << "\t\t\t||\t%%   %%  %%  %%  %%  %%  %%  %%  %%  %%  %%      %%   %%  %%      %%  %%    %%    \t||" << endl;
+    cout << "\t\t\t||\t%%   %%  %%  %%  %%  %%  %%  %%   %%%%   %%%%%%  %%   %%  %%%%%%  %%  %%    %%    \t||" << endl;
+    cout << "\t\t\t||\t\t\t\t\t\t\t\t\t\t\t\t||"<<endl;
 
-    cout << "%%   %%   %%%%   %%  %%   %%%%    %%%%   %%%%%%  %%   %%  %%%%%%  %%  %%  %%%%%% " << endl;
-    cout << "%%% %%%  %%  %%  %%% %%  %%  %%  %%      %%      %%% %%%  %%      %%% %%    %%   " << endl;
-    cout << "%% % %%  %%%%%%  %% %%%  %%%%%%  %% %%%  %%%%    %% % %%  %%%%    %%%%%     %%    " << endl;
-    cout << "%%   %%  %%  %%  %%  %%  %%  %%  %%  %%  %%      %%   %%  %%      %%  %%    %%   " << endl;
-    cout << "%%   %%  %%  %%  %%  %%  %%  %%   %%%%   %%%%%%  %%   %%  %%%%%%  %%  %%    %%   " << endl;
-    cout << endl;
-
-    cout << "  %%%%   %%  %%   %%%%   %%%%%%  %%%%%%  %%   %%" << endl;
-    cout << " %%       %%%%   %%        %%    %%      %%% %%%" << endl;
-    cout << "  %%%%     %%     %%%%     %%    %%%%    %% % %%" << endl;
-    cout << "     %%    %%        %%    %%    %%      %%   %%" << endl;
-    cout << "  %%%%     %%     %%%%     %%    %%%%%%  %%   %%" << endl;
-    cout << endl;
+    cout << "\t\t\t||\t\t\t  %%%%   %%  %%   %%%%   %%%%%%  %%%%%%  %%   %%\t\t\t||" << endl;
+    cout << "\t\t\t||\t\t\t %%       %%%%   %%        %%    %%      %%% %%%\t\t\t||" << endl;
+    cout << "\t\t\t||\t\t\t  %%%%     %%     %%%%     %%    %%%%    %% % %%\t\t\t||" << endl;
+    cout << "\t\t\t||\t\t\t     %%    %%        %%    %%    %%      %%   %%\t\t\t||" << endl;
+    cout << "\t\t\t||\t\t\t  %%%%     %%     %%%%     %%    %%%%%%  %%   %%\t\t\t||" << endl;
+    cout << "\t\t\t||\t\t\t\t\t\t\t\t\t\t\t\t||"<<endl;
+    cout << "\t\t\t||\t\t\t\t\t\t\t\t\t\t\t\t||"<<endl;
+    cout << "\t\t\t||==============================================================================================||"<<endl;
 }
 
 void display_thank_you(){
-    cout<<" _____   _   _      _      _   _   _  __   __   __   ___    _   _ "<<endl;
-    cout<<"|_   _| | | | |    / \\    | \\ | | | |/ /   \\ \\ / /  / _ \\  | | | |"<<endl;
-    cout<<"  | |   | |_| |   / _ \\   |  \\| | | ' /     \\ V /  | | | | | | | |"<<endl;
-    cout<<"  | |   |  _  |  / ___ \\  | |\\  | | . \\      | |   | |_| | | |_| |"<<endl;
-    cout<<"  |_|   |_| |_| /_/   \\_\\ |_| \\_| |_|\\_\\     |_|    \\___/   \\___/ \n"<<endl;
-    cout<<"==================================================================\n\n";
+     cout<<"\n\n\t\t\t************************************************************************"<<endl;
+     cout<<"\t\t\t*\t\t\t\t\t\t\t\t       *"<<endl;
+     cout<<"\t\t\t*\t\t\t\t\t\t\t\t       *"<<endl;
+    cout<<"\t\t\t*   _____   _   _      _      _   _   _  __   __   __   ___    _   _   *"<<endl;
+    cout<<"\t\t\t*  |_   _| | | | |    / \\    | \\ | | | |/ /   \\ \\ / /  / _ \\  | | | |  *"<<endl;
+    cout<<"\t\t\t*    | |   | |_| |   / _ \\   |  \\| | | ' /     \\ V /  | | | | | | | |  *"<<endl;
+    cout<<"\t\t\t*    | |   |  _  |  / ___ \\  | |\\  | |   \\      | |   | |_| | | |_| |  *"<<endl;
+    cout<<"\t\t\t*    |_|   |_| |_| /_/   \\_\\ |_| \\_| |_|\\_\\     |_|    \\___/   \\___/   *"<<endl;
+    cout<<"\t\t\t*\t\t\t\t\t\t\t\t       *"<<endl;
+    cout<<"\t\t\t*\t\t\t\t\t\t\t\t       *"<<endl;
+    cout<<"\t\t\t************************************************************************\n\n";
                                                                    
 }
 
